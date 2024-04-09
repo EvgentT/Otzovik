09e3dc6db4dceff4aceb0c008465633214ae3819
Евгений <136182570+EvgentT@users.noreply.github.com>
2024-04-09 16:50:38 +0300

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,11 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField, FileField
 from wtforms.validators import DataRequired, Length
+from flask_wtf.file import FileAllowed, FileRequired
 
 
 
 class ReviewForm(FlaskForm):
-    title = StringField('Название', validators=[DataRequired(message="Поле не должно быть пустым"),
-                                                Length(max=255, message='Введите заголовок длиной до 255 символов')])
-    text = TextAreaField('Текст', validators=[DataRequired(message="Поле не должно быть пустым")])
-    submit = SubmitField('Добавить')
+    name = StringField('Ваше имя',
+                       validators=[DataRequired(message="Поле не должно быть пустым")])
+    text = TextAreaField('Текст отзыва',
+                         validators=[DataRequired(message="Поле не должно быть пустым")])
+    score = SelectField('Оценка',
+                        choices=[(i, i) for i in range(1, 11)])
+    submit = SubmitField('Добавить отзыв')
+
+class MovieForm(FlaskForm):
+    title = StringField('Название',
+                        validators=[DataRequired(message="Поле не должно быть пустым")])
+    description = TextAreaField('Описание',
+                                validators=[DataRequired(message="Поле не должно быть пустым")])
+    image = FileField('Изображение', validators=[FileRequired(message="Поле не должно быть пустым"),
+                                                 FileAllowed(['jpg', 'jpeg', 'png'], message="Неверный формат файла")])
+    submit = SubmitField('Добавить фильм')
